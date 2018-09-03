efa75a8ea791ec4cdf4f33362cd11ff08a67dbf6
zanej <zanejgr@gmail.com>
2018-09-03 18:08:14 +0000

    more argument error checking

--- project0.c
+++ project0.c
@@ -1,8 +1,16 @@
 #include<stdio.h>
 #include<string.h>
 int main(int argc, char **argv){
+	//check arguments
 	if(argc != 4){
-		printf("ERROR: invalid arguments");
+		printf("ERROR: invalid number of arguments");
+		return(1);
+	}
+	if(argv[1][2]!='\0'||argv[2][2]!='\0'){
+		printf("ERROR: At least one argument is invalid");
 		return(1);
 	}	
+
+	
+
 }

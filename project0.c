6ab76731ccdf526042fef25cf894642dfb78e643
zanej <zanejgr@gmail.com>
2018-09-03 19:26:46 +0000

    created program logic

--- project0.c
+++ project0.c
@@ -2,15 +2,27 @@
 #include<string.h>
 int main(int argc, char **argv){
 	//check arguments
-	if(argc != 4){
+	if(argc > 4){
 		printf("ERROR: invalid number of arguments");
 		return(1);
 	}
-	if(argv[1][2]!='\0'||argv[2][2]!='\0'){
+	if(strlen(argv[1])!=strlen(argv[2])&&strlen(argv[2])!=1){
 		printf("ERROR: At least one argument is invalid");
+		printf("%s",strcat(argv[1],strcat(argv[2],argv[3])));
 		return(1);
 	}	
 
-	
+	//array holds counts of ascii table values
+	int counts[255];
+	for(int i = 0; i < 255; i++)
+		counts[i]=0;
+
+	//iterate over argv to count elems
+	for(int i = 0;i<strlen(argv[3]);i++){
+		counts[(int)(argv[3][i])]++;
+	}	
+
+	for(int i = 0; i < 255; i++)
+		printf("%c, %i \n",(char)(i),counts[i]);
 
 }
